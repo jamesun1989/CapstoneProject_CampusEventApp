2814d51eaf4326c30810926d8517baa479539aa7
jamesun1989 <jamesun1989@gmail.com>
2018-06-07 14:15:28 -0700

    fix description problem

--- Project/CommunityEvents/app/modules/home/scenes/Discoverdetails/Sports.js
+++ Project/CommunityEvents/app/modules/home/scenes/Discoverdetails/Sports.js
@@ -109,7 +109,7 @@ export default class ListView extends Component {
               <Text style={styles.textTitleView} >{item.title}</Text>
               <Text style={styles.textView} >{item.description}</Text>
               <Text style={styles.textTitleView} >{item.date}</Text>
-              <Button buttonStyle={{backgroundColor: '#ff4d4d', width: 100, height:30}} title={ this.state.inFavourites ? "UNFOLLOW" : "FOLLOW"}  onPress={() => this.getfavoriteEvents(item.id, item.title, item.description, item.date, item.image)}/>
+              <Button buttonStyle={{backgroundColor: '#ff4d4d', width: 100, height:30}} title='Follow'  onPress={() => this.getfavoriteEvents(item.id, item.title, item.description, item.date, item.image)}/>
               <Spacer size={15} />
               </View>
             </View>
